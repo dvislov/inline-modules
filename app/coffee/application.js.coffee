021eeaf715ebada79b8bf3dd02d14857b302f4e6
Dmitry Vislov <vdv73rus@gmail.com>
2014-11-02 22:40:39 +0300

    FAQ accordeon script added.

--- app/coffee/application.js.coffee
+++ app/coffee/application.js.coffee
@@ -1,1 +1,14 @@
-console.log 'This is coffee script success test!'
+# FAQ accordeon
+
+$(document).ready ->
+  $('.m-faq-header').click ->
+
+    if ($(this).hasClass('active'))
+      $(this).removeClass('active')
+      $(this).next('.m-faq-content').hide(400)
+    else
+      $('.m-faq-header').removeClass('active')
+      $('.m-faq-content').hide()
+
+      $(this).addClass('active')
+      $(this).next('.m-faq-content').show(400)
--- app/compile/js/application.js
+++ app/compile/js/application.js
@@ -1,4 +1,16 @@
 (function() {
-  console.log('This is coffee script success test!');
+  $(document).ready(function() {
+    return $('.m-faq-header').click(function() {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        return $(this).next('.m-faq-content').hide(400);
+      } else {
+        $('.m-faq-header').removeClass('active');
+        $('.m-faq-content').hide();
+        $(this).addClass('active');
+        return $(this).next('.m-faq-content').show(400);
+      }
+    });
+  });
 
 }).call(this);
